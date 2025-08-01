5923c5a028e3e7302c455d9d17a1c549caebe979
Durag Pal Singh <duragpalsingh05@gmail.com>
2025-08-01 21:50:57 +0530

    [FIX] : Adding Proper Constants

--- utils/AIModal.tsx
+++ utils/AIModal.tsx
@@ -1,14 +1,15 @@
 const {
   GoogleGenerativeAI,
-  
+  HarmCategory,
+  HarmBlockThreshold,
 } = require("@google/generative-ai");
 
-const apiKey = process.env.GOOGLE_GEMINI_API_KEY;
+const apiKey = process.env.NEXT_PUBLIC_GOOGLE_GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
   model: "gemini-2.5-flash",
-});
+}); 
 
 const generationConfig = {
   temperature: 1,
